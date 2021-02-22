e2015c4693663ca7fcdbb25495b2ddb8bc31f326
Anahi <aon68769@udelasalle.edu.mx>
2021-02-21 20:43:43 -0600

    Pagina Final 3.0

--- practica1/mapa.js
+++ practica1/mapa.js
@@ -2,7 +2,7 @@ AOS.init();
 
 function iniciaMapa() {
       var coordenadas = {
-            lat : 21.152639, lng : -101.711598
+            lat : 21.168422320531967, lng : -101.73453418359776
       }
 
       var map = new google.maps.Map(document.getElementById('mapa'),
