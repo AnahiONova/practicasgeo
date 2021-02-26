c38c627dc4b45a9289554b25bc97719b2a36ac1b
Anahi <aon68769@udelasalle.edu.mx>
2021-02-26 17:47:43 -0600

    Practica 4 Completa

--- practica4/index.js
+++ practica4/index.js
@@ -5,4 +5,12 @@ function obtieneUbicacion(){
   else {
     mapa.innerHTML = "El navegador no dispone de la capacidad de geolocalización"
   }
+
+  function showPosition(posicion){
+    var coordenadas = posicion.coords.latitude + "," + posicion.coords.longitude;
+    console.log(coordenadas);
+
+    var imagenmapa = document.getElementById("imagenmapa");
+    imagenmapa.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + coordenadas + "&zoom=20&size=800x800&key=AIzaSyAv-m46ebIV9fqkE2fIBGeWunmGmBm2R24";
+  }
 }
