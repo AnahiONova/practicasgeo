e068735f8a0de4f9fdf180778b49aa473bcd6047
Anahi <aon68769@udelasalle.edu.mx>
2021-03-03 20:04:48 -0600

    Practica 5 cambio

--- practica5/index.js
+++ practica5/index.js
@@ -8,4 +8,15 @@ function iniciaMapa() {
 
   const mapa = document.getElementById("map")
   const map = new google.maps.Map(mapa,propiedades)
+
+  let posicion = {
+    lat : 19.393906868527527, 
+    lng : -99.17455488787517
+  }
+
+  const marker = new google.maps.Marker({
+    position: posicion,
+    map,
+    title: "Marcador"
+  });
 }
