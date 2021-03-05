759f7387c4b8607b19f43259247af5eac5e2aed0
Anahi <aon68769@udelasalle.edu.mx>
2021-03-05 16:39:02 -0600

    Practica 5 3.0

--- practica5/index.js
+++ practica5/index.js
@@ -13,6 +13,7 @@ function iniciaMapa() {
   const mapa = document.getElementById("map")
   const map = new google.maps.Map(mapa,propiedades)
 
+
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(position =>{
       let posicion = {
@@ -26,10 +27,14 @@ function iniciaMapa() {
         title: "Marcador"
       });
 
+      map.setCenter(posicion);
+
       const PosMarker2 = {
-        position: posicion,
-        msp,
-        title: "Marcador2"
+        position: {
+          lat : 19.393906868527527, lng : -99.17455488787517
+        },
+        map,
+        title: "Marcador 2"
       }
 
       const marker2 = new google.maps.Marker(PosMarker2);
@@ -41,8 +46,6 @@ function iniciaMapa() {
       marker2.addListener("click", ()=>{
         infowindow.open(map,marker);
       });
-
-      map.setCenter(posicion);
 
       const infowindow = new google.maps.InfoWindow({
         content : info
