aec0dd13c9964fce09b2fbc7fa11c6c9324fd0aa
Anahi <aon68769@udelasalle.edu.mx>
2021-03-11 22:02:52 -0600

    FETCH

---
+++ FETCH_practica2/practica2.js
@@ -0,0 +1,22 @@
+var paises = document.getElementById("paises");
+fetch('https://corona.lmao.ninja/v3/covid-19/countries')
+.then(function(response){
+
+  response.json().then(function(datos){
+    datos.forEach(registro => {
+      console.log(registro.country);
+      let renglon = document.createElement("div");
+      renglon.className = "row border bg-info";
+      paises.appendChild(renglon);
+
+      let columna = document.createElement("div");
+      columna.className = "col-12";
+      renglon.appendChild(columna);
+
+      let nombre = document.createElement("p");
+      nombre.textContent = "País: " + registro.country + ", casos: " + registro.cases;
+      columna.appendChild(nombre);
+    });
+  });
+
+});
