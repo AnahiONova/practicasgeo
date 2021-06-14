f006b1fa993a967b303e219ad838892fb035f5da
Anahi <aon68769@udelasalle.edu.mx>
2021-06-14 01:53:21 -0500

    Proyecto Final Auth

---
+++ Proyecto_Final/scripts/autorizacion.js
@@ -0,0 +1,14 @@
+const formaIngresar = document.getElementById("formaIngresar");
+
+formaIngresar.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  let correo = formaIngresar['correo'].value;
+  let contrasena = formaIngresar['contrasena'].value;
+
+  auth.signInWithEmailAndPassword(correo,contrasena).then(cred =>{
+    console.log('Entro');
+  }).catch(err =>{
+    console.log(err)
+  });
+})
