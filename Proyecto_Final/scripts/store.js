b0113749af4b05faeee14dc25734b467c5aed27b
Anahi <aon68769@udelasalle.edu.mx>
2021-06-14 22:08:27 -0500

    Proyecto Final Auth 28

--- Proyecto_Final/scripts/store.js
+++ Proyecto_Final/scripts/store.js
@@ -4,6 +4,17 @@ const datosdelacuenta = document.querySelector('.datosdelacuenta');
 
 const configurarMenu = (user) => {
   if(user){
+    db.collection('usuarios').doc(user.uid).get().then(doc =>{
+      const html = `
+        <p>Nombre: ${doc.data().nombre}</p>
+        <p>Correo: ${user.email}</p>
+        <p>Telefono: ${doc.data().telefono}</p>
+        <p>Direccion: ${doc.data().direccion}</p>
+      `;
+
+      datosdelacuenta.innerHTML = html;
+
+    });
     listaloggedin.forEach(item => item.style.display = 'block');
     listaloggedout.forEach(item => item.style.display = 'none');
   }else{
